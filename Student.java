921e3864d661cb3e787b8e29e8404f5b8e555609
Gopika G <rajeswariganesan13@gmail.com>
2025-07-26 16:22:15 +0530

    Update Student.java

--- Student.java
+++ Student.java
@@ -1,1 +1,18 @@
-// Placeholder content for Student.java
+public class Student {
+    int id;
+    String name;
+    String email;
+    String course;
+
+    public Student(int id, String name, String email, String course) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.course = course;
+    }
+
+    public String toString() {
+        return "ID: " + id + ", Name: " + name + ", Email: " + email + ", Course: " + course;
+    }
+}
+
